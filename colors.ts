59ad460e2f94a83e46d7e2d11d23514b1a30ef73
Juwan Wheatley <datwheat@gmail.com>
2020-08-14 02:53:19 -0700

    make colors more subtle

--- colors.ts
+++ colors.ts
@@ -1,9 +1,12 @@
 export const black = "#010209";
-export const white15 = "rgba(255,255,255,0.15)";
+export const white5 = "rgba(255,255,255,0.05)";
 export const white10 = "rgba(255,255,255,0.10)";
+export const white15 = "rgba(255,255,255,0.15)";
+export const white30 = "rgba(255,255,255,0.30)";
 export const white40 = "rgba(255,255,255,0.40)";
 export const white80 = "rgba(255,255,255,0.80)";
-export const darkGray = "#242424";
+export const white95 = "rgba(255,255,255,0.95)";
+export const darkGray = "#0E0F15";
 export const white = "#ffffff";
 export const expoBlurple = "#4630EB";
 export const goodweebsPurple = "#651FFF";
