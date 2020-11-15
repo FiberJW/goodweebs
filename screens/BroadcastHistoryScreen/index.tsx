bf28268a039f383b3a036fb42a0f43bd660978d2
Juwan Wheatley <datwheat@gmail.com>
2020-11-15 15:41:53 -0500

    move AiringItem to PressableOpacity

--- screens/BroadcastHistoryScreen/index.tsx
+++ screens/BroadcastHistoryScreen/index.tsx
@@ -1,23 +1,21 @@
 import { useFocusEffect } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import { formatDistance } from "date-fns";
 import React, { useMemo, useState, useCallback } from "react";
 import { RefreshControl } from "react-native";
 
 import { EmptyState } from "yep/components/EmptyState";
 import { Header } from "yep/components/Header";
-import { PosterAndTitle } from "yep/components/PosterAndTitle";
 import {
   AiringNotificationFragmentFragment,
   useGetAnimeNotificationsQuery,
 } from "yep/graphql/generated";
-import { useNow } from "yep/hooks/helpers";
 import { RootStackParamList } from "yep/navigation";
 import { getString, StringCase } from "yep/strings";
 import { takimoto } from "yep/takimoto";
 import { darkTheme } from "yep/themes";
-import { Manrope } from "yep/typefaces";
-import { notEmpty, getTitle } from "yep/utils";
+import { notEmpty } from "yep/utils";
+
+import { AiringItem } from "./AiringItem";
 
 type Props = {
   navigation: StackNavigationProp<RootStackParamList>;
@@ -87,65 +85,6 @@ export function BroadcastHistoryScreen({ navigation }: Props) {
     </OuterContainer>
   );
 }
-
-type AiringItemProps = {
-  notification: AiringNotificationFragmentFragment;
-  navigation: StackNavigationProp<RootStackParamList>;
-};
-
-function AiringItem({ navigation, notification }: AiringItemProps) {
-  const now = useNow();
-  const posterUri = notification.media?.coverImage?.large;
-
-  return (
-    <AiringItemContainer
-      onPress={() => {
-        navigation.navigate("Details", { id: notification.animeId });
-      }}
-    >
-      {posterUri ? <PosterAndTitle uri={posterUri} size="tiny" /> : null}
-      <AiringItemTextContainer>
-        <AiringItemTitle numberOfLines={2}>
-          {getTitle(notification.media?.title)}
-        </AiringItemTitle>
-        <AiringItemText numberOfLines={1}>
-          Episode {notification.episode} aired{" "}
-          {formatDistance((notification.createdAt ?? 0) * 1000, now, {
-            addSuffix: true,
-          })}
-          .
-        </AiringItemText>
-      </AiringItemTextContainer>
-    </AiringItemContainer>
-  );
-}
-
-export const AiringItemTextContainer = takimoto.View({
-  paddingHorizontal: 8,
-  flex: 1,
-  justifyContent: "space-between",
-});
-
-export const AiringItemTitle = takimoto.Text({
-  fontFamily: Manrope.semiBold,
-  color: darkTheme.text,
-  fontSize: 16,
-  marginBottom: 4,
-});
-
-const AiringItemContainer = takimoto.TouchableOpacity({
-  padding: 8,
-  borderRadius: 8,
-  backgroundColor: darkTheme.listItemBackground,
-  flexDirection: "row",
-});
-
-const AiringItemText = takimoto.Text({
-  fontFamily: Manrope.regular,
-  fontSize: 12.8,
-  color: darkTheme.text,
-  maxWidth: "100%",
-});
 
 const OuterContainer = takimoto.View({
   flex: 1,
