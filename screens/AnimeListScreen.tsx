0486221456d308d7f93e89c601aa0fac921dd238
Juwan Wheatley <datwheat@gmail.com>
2023-08-30 14:15:37 -0400

    update query simplification plan

--- screens/AnimeListScreen.tsx
+++ screens/AnimeListScreen.tsx
@@ -47,7 +47,7 @@ export function AnimeListScreen({ navigation }: Props) {
   const { accessToken, setAccessToken } = useAccessToken();
 
   const [, , promptAsync] = useAniListAuthRequest();
-  // TODO: combine queries
+  // TODO: save userId to async storage instead of fetching
   const { loading: loadingViewer, data: viewerData } = useGetViewerQuery({
     skip: !accessToken,
   });
