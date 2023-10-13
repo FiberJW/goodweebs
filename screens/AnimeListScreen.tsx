417f09e7ab6d521c914b68fa7a8536256be065b5
Juwan Wheatley <datwheat@gmail.com>
2023-10-13 19:51:47 -0400

    w

--- screens/AnimeListScreen.tsx
+++ screens/AnimeListScreen.tsx
@@ -51,7 +51,7 @@ export function AnimeListScreen({ navigation }: Props) {
   const { loading: loadingViewer, data: viewerData } = useGetViewerQuery({
     skip: !accessToken,
   });
-  // hiiieeeeede
+  // hiiieeeeedew
 
   const {
     loading: loadingAnimeList,
