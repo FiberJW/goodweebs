f45f97b6666020de2778d123cca1e72842a31671
Juwan Wheatley <datwheat@gmail.com>
2023-11-06 14:01:39 -0500

    another bump for prod

--- apps/goodweebs/strings.ts
+++ apps/goodweebs/strings.ts
@@ -7,7 +7,7 @@ export enum StringCase {
   SOURCE,
 }
 
-// hi chat
+// hi
 const _en_us = {
   AniListAuthAttribution: "via AniList",
   tagline: "An anime tracking app powered by AniList and Expo.",
