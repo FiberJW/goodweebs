5144db7ec7462fe35e41806ea116a9dbd1b47745
Juwan Wheatley <datwheat@gmail.com>
2024-02-12 12:48:43 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -20,7 +20,7 @@ enableScreens();
 
 Sentry.init({
   dsn: "https://b2756b0df548451d98707d024aff00d1@o58038.ingest.sentry.io/5248224",
-  debug: __DEV__, // If `byebye`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  debug: __DEV__, // If `e`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
 });
 
 SplashScreen.preventAutoHideAsync();
