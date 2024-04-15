7a5bf882fa774b2997fcb65f8b7ddbc104424c70
Juwan Wheatley <datwheat@gmail.com>
2024-04-15 16:48:01 -0400

    add the correct updates url for staging

--- app.config.ts
+++ app.config.ts
@@ -2,15 +2,27 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 
 const IS_DEV = process.env.APP_VARIANT === "development";
 
+const name = (() => {
+  if (IS_DEV) return "Goodweebs (Dev)";
+  if (process.env.EXPO_STAGING) return "Goodweebs (Staging)";
+  return "Goodweebs";
+})();
+
+const scheme = (() => {
+  if (IS_DEV) return "goodweebs-dev";
+  if (process.env.EXPO_STAGING) return "goodweebs-staging";
+  return "goodweebs";
+})();
+
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: IS_DEV ? "Goodweebs (Dev)" : "Goodweebs",
+  name,
   slug: "goodweebs",
   platforms: ["ios", "android", "web"],
   version: "0.0.15",
   orientation: "portrait",
   icon: IS_DEV ? "./assets/launch/icon-dev.png" : "./assets/launch/icon.png",
-  scheme: IS_DEV ? "goodweebs-dev" : "goodweebs",
+  scheme,
   privacy: "unlisted",
   backgroundColor: "#010209",
   splash: {
@@ -21,7 +33,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   owner: "fiberjw",
   updates: {
     fallbackToCacheTimeout: 30000,
-    url: "https://u.expo.dev/21cb2a71-d249-4289-abb6-3b9b39e3b0a3",
+    url: process.env.EXPO_STAGING
+      ? "https://staging-u.expo.dev/4678c342-b7f2-4911-ae87-0c5c0de6c188"
+      : "https://u.expo.dev/21cb2a71-d249-4289-abb6-3b9b39e3b0a3",
   },
   assetBundlePatterns: ["**/*"],
   ios: {
