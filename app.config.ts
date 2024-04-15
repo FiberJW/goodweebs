64d03f4b7cb75d8eb002fec12614f48dd53f4512
Juwan Wheatley <datwheat@gmail.com>
2024-04-15 16:44:40 -0400

    save

--- app.config.ts
+++ app.config.ts
@@ -87,6 +87,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
             organization: "juwan-wheatley",
           },
         ],
+    "expo-router",
     [
       "expo-updates",
       {
---
+++ app/(tabs)/anime.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Tab() {
+  return (
+    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+      <Text>Anime</Text>
+    </View>
+  );
+}
