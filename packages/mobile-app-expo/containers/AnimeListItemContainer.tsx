cebc3d29e97d314f70af09f66e491adc26066d37
Juwan Wheatley <datwheat@gmail.com>
2024-07-26 23:38:32 -0400

    warudo

--- packages/mobile-app-expo/containers/AnimeListItemContainer.tsx
+++ packages/mobile-app-expo/containers/AnimeListItemContainer.tsx
@@ -57,7 +57,7 @@ export function AnimeListItemContainer({
     skip: !accessToken,
   });
 
-  // hello
+  // hello world
 
   const updateProgressDebounced = useDebouncedMutation<
     UpdateProgressMutation,
