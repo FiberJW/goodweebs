a4663d14feb509fd6c9672151048877987d73b63
Juwan Wheatley <datwheat@gmail.com>
2026-01-19 02:03:55 +0900

    revert native tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,7 @@
-import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
+import { Tabs } from "expo-router";
 import React from "react";
-import { Platform } from "react-native";
+import { Image } from "react-native";
 
-import { officialSiteFallback } from "yep/colors";
 import { darkTheme } from "yep/themes";
 import { useAccessToken } from "yep/useAccessToken";
 
@@ -10,28 +9,64 @@ export default function TabsLayout() {
   const { accessToken } = useAccessToken();
 
   return (
-    <NativeTabs
-      backgroundColor={darkTheme.navBackground}
-      tintColor={Platform.OS === "ios" ? officialSiteFallback : darkTheme.text}
-      indicatorColor={darkTheme.button}
-      disableIndicator
-      labelVisibilityMode="unlabeled"
-      // TODO: figure out how to configure a background color and inset for < iOS 26 native tabs
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: darkTheme.navBackground,
+          borderTopColor: "transparent",
+        },
+        tabBarActiveTintColor: darkTheme.text,
+        tabBarInactiveTintColor: darkTheme.listItemBackground,
+      }}
     >
-      <NativeTabs.Trigger name="anime">
-        <Icon src={require("yep/assets/icons/navigation/anime-tab.png")} />
-        <Label>Anime</Label>
-      </NativeTabs.Trigger>
-
-      <NativeTabs.Trigger name="discover">
-        <Icon src={require("yep/assets/icons/navigation/discover-tab.png")} />
-        <Label>Discover</Label>
-      </NativeTabs.Trigger>
-
-      <NativeTabs.Trigger hidden={!accessToken} name="profile">
-        <Icon src={require("yep/assets/icons/navigation/profile-tab.png")} />
-        <Label>Profile</Label>
-      </NativeTabs.Trigger>
-    </NativeTabs>
+      <Tabs.Screen
+        name="anime"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              style={{
+                tintColor: color,
+                height: size,
+                width: size,
+              }}
+              source={require("yep/assets/icons/navigation/anime-tab.png")}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="discover"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              style={{
+                tintColor: color,
+                height: size,
+                width: size,
+              }}
+              source={require("yep/assets/icons/navigation/discover-tab.png")}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          href: accessToken ? "/profile" : null,
+          tabBarIcon: ({ color, size }) => (
+            <Image
+              style={{
+                tintColor: color,
+                height: size,
+                width: size,
+              }}
+              source={require("yep/assets/icons/navigation/profile-tab.png")}
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
