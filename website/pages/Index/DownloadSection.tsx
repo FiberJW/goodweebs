67769fc2a0ebd6d8047425c0f0872638785ecf37
Juwan Wheatley <datwheat@gmail.com>
2026-07-07 05:51:39 +0900

    Use expo-image on the website landing page (#56)

--- website/pages/Index/DownloadSection.tsx
+++ website/pages/Index/DownloadSection.tsx
@@ -1,5 +1,6 @@
+import { Image, type ImageStyle } from "expo-image";
 import React from "react";
-import { View, Text, Image, StyleSheet, ImageStyle } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 import { useBreakpoints } from "yep/hooks/helpers";
 import { websiteTheme } from "yep/themes";
--- website/pages/Index/Footer.tsx
+++ website/pages/Index/Footer.tsx
@@ -1,5 +1,6 @@
+import { Image } from "expo-image";
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 import { useBreakpoints } from "yep/hooks/helpers";
 import { websiteTheme } from "yep/themes";
