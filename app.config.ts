8f1756248960eb64180fedf393c8217a5c3f3ac4
Juwan Wheatley <datwheat@gmail.com>
2026-07-07 04:28:33 +0700

    another bump

--- app.config.ts
+++ app.config.ts
@@ -39,7 +39,7 @@ const androidForegroundImage = (() => {
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name,
-  version: "0.0.19",
+  version: "0.0.20",
   slug: "goodweebs",
   platforms: ["ios", "android", "web"],
   orientation: "portrait",
